f2b6d85db0e54d812064014d3eb35604a6a09ab2
Derek Keeler <derek@suchcool.ca>
2021-08-28 22:54:51 -0700

    Only open ports if the firewall is running

--- windows/wreckfest-dedicated-server/scripts/Open-WreckfestPorts.ps1
+++ windows/wreckfest-dedicated-server/scripts/Open-WreckfestPorts.ps1
@@ -1,14 +1,17 @@
-# Protocol:Port to open in the Windows Firewall
-$ProtocolsAndPorts = @{
-    "TCP"=@(27015);
-    "UDP"=@(27015, 33540)
-}
+if (( Get-Service -Name "Windows Defender Firewall").Status -eq "Running") {
 
-# Set all inbound ports & protocols to 'allow'
-$ProtocolsAndPorts.Keys | ForEach-Object {
-    $protocol = $PSItem
-    $ProtocolsAndPorts[$protocol] | ForEach-Object {
-        $port = $PSItem
-        New-NetFirewallRule -DisplayName ""Wreckfest Dedicated Server $protocol $port"" -Direction inbound -Profile Any -Action Allow -LocalPort $port -Protocol $protocol
+    # Protocol:Port to open in the Windows Firewall
+    $ProtocolsAndPorts = @{
+        "TCP"=@(27015);
+        "UDP"=@(27015, 33540)
+    }
+
+    # Set all inbound ports & protocols to 'allow'
+    $ProtocolsAndPorts.Keys | ForEach-Object {
+        $protocol = $PSItem
+        $ProtocolsAndPorts[$protocol] | ForEach-Object {
+            $port = $PSItem
+            New-NetFirewallRule -DisplayName "Wreckfest Dedicated Server $protocol $port" -Direction inbound -Profile Any -Action Allow -LocalPort $port -Protocol $protocol
+        }
     }
 }
